37b26c241b4334a520cd60442b8d5d5efda2ab19
Hovhannes Ghabanyan <ghabanyan@icloud.com>
2021-02-06 18:27:27 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBytes();
+            Console.ReadLine();
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static void ProcessBytes()
+        {
+            byte bl = 100;
+            byte b2 = 250;
+
+
+            unchecked
+            {
+                byte sum = (byte)(bl + b2);
+                Console.WriteLine($"sum = {sum}");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
